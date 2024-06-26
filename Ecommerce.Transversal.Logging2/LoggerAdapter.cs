82c2305da16e7d9ed52f7a3d7c49472b3d99c6c7
Sarah Lopez <93948109+saralop4@users.noreply.github.com>
2024-06-26 00:04:40 -0500

    correcciones de codigo

---
+++ Ecommerce.Transversal.Logging2/LoggerAdapter.cs
@@ -0,0 +1,39 @@
+﻿using Ecommerce.Common.Interfaces;
+using Microsoft.Extensions.Logging;  //componente nugget
+
+namespace Ecommerce.Transversal.Logging
+{
+    public class LoggerAdapter<T> : IAppLogger<T> 
+    {
+        private readonly ILogger<T> _logger;
+
+        /*En la consola de visual studio se muestan los logs que hemos generado con cada uno de los metodos,
+         estos metodos son utilizados en los controladores o tambien pueden ser uilizados en la logica
+        ya que son para mostrar en consola mensajes de exitos o errores */
+
+        /*Estos logs se pueden persistir en archivos de textos, base de datos, archivos xml o en lo que
+         uno desee o crea conveniente*/
+
+
+        public LoggerAdapter(ILoggerFactory loggerFactory) //esta interfaz nos permite crear los sistemas de loggin y trazabilidad de las excepciones
+        {
+            _logger= loggerFactory.CreateLogger<T>();   
+        }
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);  //estos metodos son propios del componente nugget que se instaló
+
+        }
+      public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);  
+
+        }
+      public void LogError(string message, params object[] args)
+        {
+            _logger.LogError(message, args);
+
+        }
+
+    }
+}

c8bc38abe1d5383c4ca4b5ff0d9cfdf5e7a35ed8
saralop4 <saralopezjb16@gmail.com>
2024-07-31 21:34:12 -0500

    aplicando el patron healtcheck

---
+++ Ecommerce.Services.WebApi/Modules/HealthChecks/HealthCheckCustom.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Ecommerce.Services.WebApi.Modules.HealthChecks
+{
+    //HealthCheck Personalizados
+    public class HealthCheckCustom : IHealthCheck
+    {
+        private readonly Random _random = new ();
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            //esto no se debe aplicar en la vida real ya que es algo fictisio 
+            var responseTime = _random.Next(1, 300); //simulando con un valor aleatorio el tiempo de respuesta de las diversas dependencias
+
+            if(responseTime < 100 )
+            {
+                return Task.FromResult(HealthCheckResult.Healthy("Healthy result from HealthCheckCustom"));
+
+            }
+            else if (responseTime < 200)
+            {
+                return Task.FromResult(HealthCheckResult.Degraded("Degraded result from HealthCheckCustom"));
+            }
+            return Task.FromResult(HealthCheckResult.Unhealthy("Unhealthy result from HealthCheckCustom"));
+                 
+        }
+    }
+}
